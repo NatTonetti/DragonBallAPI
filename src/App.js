55267dcf7cad4140ca726f96cb440d2da5f7a10b
NatTonetti <tonettinatacha@gmail.com>
2022-11-15 23:41:59 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [anime, setAnime] = useState()
+
+
+  useEffect(() => {
+    fetch(`https://kitsu.io/api/edge/anime?filter[text]=dragonball`)
+      .then(res => res.json())
+      .then(res => {
+        setAnime(res)
+      })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='main-container'>
+      {anime !== undefined ?
+        <div class='sub-container'>
+          {anime.data.slice(0, 2).map((info, id) => (
+            <div class='map-container' key={id}>
+              <h2>
+                {info.attributes.titles.en + "-" + info.attributes.titles.ja_jp}
+              </h2>
+              <div class='description-container'>
+              <h4>
+                  {info.attributes.description}
+                </h4>
+                </div>
+              
+              <div class='img-container' >
+                <img class='img-poster' src={info.attributes.posterImage.small} />
+                <img class='img-cover' src={info.attributes.coverImage.small} />
+              </div>
+            </div>
+          ))}
+        </div>
+        : null
+        }
     </div>
-  );
+  )
 }
-
-export default App;
